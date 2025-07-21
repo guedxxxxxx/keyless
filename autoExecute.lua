fb549e962dfc2d2c1e72143e167cd654d8e01f7b
guedxxxxxx <guedxrblx@gmail.com>
2025-07-21 00:46:22 -0300

    Update and rename autoExecute.lua.txt to autoExecute.lua

---
+++ autoExecute.lua
@@ -0,0 +1,18 @@
+local Players = game:GetService('Players')
+
+local TeleportCheck = false
+
+queueteleport = (syn and syn.queue_on_teleport)
+    or queue_on_teleport
+    or (fluxus and fluxus.queue_on_teleport)
+
+local function autoExecute(scripts)
+    Players.LocalPlayer.OnTeleport:Connect(function(State)
+        if not TeleportCheck and queueteleport then
+            TeleportCheck = true
+            queueteleport(scripts)
+        end
+    end)
+end
+
+return autoExecute
